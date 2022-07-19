a5f5aae4bc7e19b8ea579fa767112ef9a0ac7a2d
DopeKS <kyusang.dev@gmail.com>
2022-07-19 20:00:32 +0900

    Understand the default code for the Platter App Page 2 (플러터 앱페이지 기본 코드 이해하기 2)

---
+++ lib/dopeKs/01_Understanding_the_Plutter_Project_Folder_and_Basic_Code/understanding_the_Plutter_Project_Folder_and_Basic_Code_02/main.dart
@@ -0,0 +1,19 @@
+/*
+import 'package:flutter/material.dart';
+
+import '../understanding_the_Plutter_Project_Folder_and_Basic_Code_02/main.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+    );
+  }
+*/
