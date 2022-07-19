0a81c23d721849f9e59888752e480cb8c40d7076
DopeKS <kyusang.dev@gmail.com>
2022-07-19 20:00:50 +0900

    Understanding the basic code of the Platter App Page 3 (Completed Edition) [플러터 앱페이지 기본 코드 이해하기 3 (완결편)]

---
+++ lib/dopeKs/01_Understanding_the_Plutter_Project_Folder_and_Basic_Code/understanding_the_Plutter_Project_Folder_and_Basic_Code_03/main.dart
@@ -0,0 +1,43 @@
+/*
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First app'),
+      ),
+      body: Center(
+        child: Column(
+          children: const <Widget>[
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+*/
